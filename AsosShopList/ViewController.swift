cc0eb4cbd7a872867274d46709f27ec50772ebab
GenuineMas <MasalitinJW@gmail.com>
2020-10-16 02:08:19 +0300

    Initial Commit

---
+++ AsosShopList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AsosShopList
+//
+//  Created by Genuine on 16.10.2020.
+//  Copyright © 2020 Genuine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
